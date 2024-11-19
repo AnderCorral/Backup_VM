c4a00fc34746560306eb8a5b20ccb9572c604d51
AnderCorral <81016443+AnderCorral@users.noreply.github.com>
2024-11-19 18:09:24 +0100

    Add files via upload

--- backup_VM.sh
+++ backup_VM.sh
@@ -5,14 +5,14 @@ origen="/home/usuario/seguridad/"
 ssh="ander.corral2002@34.175.231.243"
 fecha=$(date -d 'today' +"%d-%m-%Y")
 fechayer=$(date -d '-1 days' +"%d-%m-%Y")
-destino="$ssh:Backups/$fecha/"
-destinoayer="$ssh:Backups/$fechayer/"
+destino="Backups/$fecha/"
+destinoayer="Backups/$fechayer/"
 
-# Crear el directorio de destino si no existe
-mkdir $destino
+# Crear el directorio de destino si no ex
+ssh "$ssh" "mkdir $destino"
 
-if [-d "$destinoayer"]; then
-	rsync -av --link-dest="$destinoayer" "$origen" "$destino"
+if ssh "$ssh" "[-d $destinoayer]"; then
+	rsync -av --link-dest="$destinoayer" "$origen" "$ssh:$destino"
 else
-	rsync -av "$origen" "$destino"
+	rsync -av "$origen" "$ssh:$destino"
 fi
